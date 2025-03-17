3ba9a215070bb65e7605852f7091e836095a5858
huynna12 <honhuhuynh1210@gmail.com>
2025-03-17 17:14:20 -0400

    update

--- PigSolver.java
+++ PigSolver.java
@@ -4,35 +4,32 @@ import java.io.IOException;
 
 public class PigSolver {
     // Instance variables
-    private double[][][] p;
-    private boolean[][][] roll;
-    private int goal;
+    private double[][][] p;       //Storing win probability for each valid stage
+    private boolean[][][] roll;   //Storing value to show if the player should roll in the stage or not
+    private int goal;             //The point goal for Pig game
 
     public PigSolver(int goal, double epsilon) {
         this.goal = goal;
         p = new double[goal][goal][goal];
         roll = new boolean[goal][goal][goal];
 
-        int iterations = 0;
-        double theta;
-        do {
-            theta = valueIterate();
-            iterations++;
-            if (iterations % 1000 == 0) {
-                System.out.println("Iteration: " + iterations + " | Theta: " + theta);
-            }
-        } while (theta >= epsilon);
+        while (valueIterate() >= epsilon);
     }
 
     private double valueIterate() {
         double theta = 0.0;
 
+        // For each state
         for (int i = 0; i < goal; i++) {
             for (int j = 0; j < goal; j++) {
                 for (int k = 0; k < goal - i; k++) {
+                    // Get the old probability
                     double oldProb = p[i][j][k];
+
+                    //Set the new probability in p 
                     p[i][j][k] = computeNewProb(i, j, k);
 
+                    // Update theta, the maximum of current theta and the difference of old and new winning probability
                     theta = Math.max(theta, Math.abs(oldProb - p[i][j][k]));
                 }
             }
@@ -54,6 +51,7 @@ public class PigSolver {
     }
 
     public double pWin(int i, int j, int k) {
+        // Return 1.0 if we reach the goal, 0.0 if the opponent do, and the winning probability of the state otherwise
         if (i + k >= goal) return 1.0;
         else if (j >= goal) return 0.0;
         else return p[i][j][k];
@@ -107,8 +105,7 @@ public class PigSolver {
     }
 
     
-    public 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         int goal = 100;
         double epsilon = 1e-9;
 
@@ -116,7 +113,5 @@ public class PigSolver {
         solver.outputPolicy();
         System.out.println("Probability that the first player will win if both play optimally: " + solver.pWin(0, 0, 0));
         solver.advise();
-
-        // solver.savePolicyToFile("policy.csv", 10);
     }
 }
