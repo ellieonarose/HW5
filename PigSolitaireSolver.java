05c6019963585f3dd69bdb17be190c6b0adbcf4d
Eliana Rose Mandelberg <elianarose123@gmail.com>
2025-03-18 16:51:58 -0400

    Update PigSolitaireSolver.java

--- PigSolitaireSolver.java
+++ PigSolitaireSolver.java
@@ -1,63 +1,150 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
 // This class is a solver for pig solitaire
-//Consider the solitaire (single player) game of Pig where a player is challenged to reach a given goal score g within n turns.
 public class PigSolitaireSolver {
 
-//Instance variables
-private int goal;
-private int turns;
+    // Instance variables
+    private int goal;
+    private int turns;
+    private Map<String, Double> memo; // Memoization cache
 
-//Constructor that takes in an int for the goal and int for the amount of turns
-public PigSolitaireSolver(int goal, int turns){
-    this.goal = goal;
-    this.turns = turns;
-}
-
-//Method that Return the probability of winning with optimal play given the current score, number of turns completed, and current turn total.
-public double pWin(int i, int j, int k) {
-    if (i >= goal) {
-        return 1;
+    // Constructor
+    public PigSolitaireSolver(int goal, int turns) {
+        this.goal = goal;
+        this.turns = turns;
+        this.memo = new HashMap<>();
     }
-    if (j >= turns) {
-        return 0;
-    }
-    
-    double pRoll = 0;
-    double pHold = 0;
+//for each i and j there is some min k to hold
+//print out comma separated grid of min hold values, import into spreadsheet
+//surface plot over values
 
-    // 1/6 of the time, the player will roll a 1 and lose their turn total
-    pRoll += 1.0/6 * (1 - pWin(i, j + 1, 0));
+    //method to compute the minimum hold value for each i and j at each score
+    public int minHold(int i, int j) {
+        int minHold = Integer.MAX_VALUE;
+        for (int k = 1; k <= 100 - i; k++) { // Ensure we don't exceed goal
+        double pRoll = (1.0 / 6) * pWin(i, j - 1, 0);
+        for (int roll = 2; roll <= 6; roll++) {
+            pRoll += (1.0 / 6) * pWin(i, j, k + roll);
+        }
+        double pHold = pWin(i + k, j - 1, 0);
 
-    // 5/6 of the time, the player will roll a number between 2 and 6
-    for (int l = 2; l <= 6; l++) {
-        pRoll += 1.0 / 6 * pWin(i, j + 1, k + l);
-    }
-
-    pHold = pWin(i + k, j + 1, 0);
-    return Math.max(pRoll, pHold);
-}
-
-//Method that determins whether or not an optimal player should roll given the current score, number of turns completed, and current turn total
-public boolean shouldRoll(int i, int j, int k) {
-    if (i >= goal) {
-        return false;
-    }
-    if (j >= turns) {
-        return false;
+        if (pHold >= pRoll) {
+            minHold = k;
+            break;  // Stop early once we find the minimum hold value
+        }
     }
 
-    double pRoll = 0;
-    double pHold = 0;
-   
-    // 1/6 of the time, the player will roll a 1 and lose their turn total
-    pRoll += 1.0/6 * (1 - pWin(i, j + 1, 0));
-
-    // 5/6 of the time, the player will roll a number between 2 and 6
-    for (int l = 2; l <= 6; l++) {
-        pRoll += 1.0 / 6 * pWin(i, j + 1, k + l);
+        return minHold;
     }
 
-    pHold = pWin(i + k, j + 1, 0);
-    return pRoll > pHold;
+    // Method to compute the probability of winning with optimal play
+    public double pWin(int i, int j, int k) {
+        // Base cases
+        // If reaching or exceeding goal, win
+        if (i + k >= goal) 
+            return 1.0;  
 
-}
+        // No turns left, lose
+        if (j == 0) 
+            return 0.0;  
+
+        // Memoization lookup
+        String state = i + "," + j + "," + k;
+        if (memo.containsKey(state)) 
+            return memo.get(state);
+
+        // Probability calculations
+        // Rolling a 1 resets turn score
+        double pRoll = (1.0 / 6) * pWin(i, j - 1, 0); 
+        // Rolling 2-6 keeps turn count
+        for (int roll = 2; roll <= 6; roll++) {
+            pRoll += (1.0 / 6) * pWin(i, j, k + roll); 
+        }
+
+        // Holding secures points
+        double pHold = pWin(i + k, j - 1, 0) ; 
+
+        double result = Math.max(pRoll, pHold);
+        memo.put(state, result); // Store result
+        return result;
+    }
+
+    // Method to determine if rolling is the best option
+    public boolean shouldRoll(int i, int j, int k) {
+       
+        // If no roll has been taken, roll
+        if(k == 0){
+            return true;
+        }
+
+        // No turns left or already won
+        if (j == 0 || i + k >= goal) 
+            return false; 
+
+        // if k is great enough to give a good chacne of winning, hold
+        // if(k > 1/5 * goal)  
+        //     return false;
+
+        double pRoll = (1.0 / 6) * pWin(i, j - 1, 0);
+        for (int roll = 2; roll <= 6; roll++) {
+            pRoll += (1.0 / 6) * pWin(i, j, k + roll);
+        }
+        double pHold = pWin(i + k, j - 1, 0) ;
+
+        // Roll if it has a higher probability of winning
+        return pRoll > pHold; 
+    }
+
+    // Main method to test the solver
+    public static void main(String[] args) {
+        PigSolitaireSolver solver = new PigSolitaireSolver(100, 11);
+        //print probability of winning
+        System.out.println("Probability of winning: " + solver.pWin(0, 11, 0));
+        Die die = new Die();
+
+        //print out the minimum hold values
+        for (int i = 0; i < 100; i++) {
+            for (int j = 10; j >=1; j--) {
+                System.out.print(solver.minHold(i, j) + ",");
+            }
+            System.out.println();
+        }
+
+        // int myScore = 0;
+        // int turnScore = 0;
+        // int remainingTurns = 10;
+
+        // while (myScore < 100 && remainingTurns > 0) {
+        //     System.out.println("\nCurrent Score: " + myScore + " | Turns Left: " + remainingTurns);
+        //     boolean rolling = solver.shouldRoll(myScore, remainingTurns, turnScore);
+
+        //     if (rolling) {
+        //         int roll = die.die();
+        //         System.out.println("Player rolled: " + roll);
+        //         if (roll == 1) {
+        //             // Rolling a 1 resets turnScore and ends turn
+        //             turnScore = 0;
+        //             remainingTurns--;
+        //             System.out.println("Rolled a 1! Lost turn points.");
+        //         } else {
+        //             turnScore += roll;
+        //         }
+        //     } else {
+        //         // Player chooses to hold
+        //         myScore += turnScore;
+        //         turnScore = 0;
+        //         remainingTurns--;
+        //         System.out.println("Player held. New Score: " + myScore);
+        //     }
+        // }
+
+        // // Determine if player wins or loses
+        // if (myScore >= 100) {
+        //     System.out.println("\nPlayer wins!");
+        // } else {
+        //     System.out.println("\nPlayer loses!");
+        // }
+    }
 }
